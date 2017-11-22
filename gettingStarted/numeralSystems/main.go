2ac055c197f6a284e714e7a4091c3b537da427ea
Feng Zhu <feng.zhu1980@gmail.com>
2017-11-22 14:43:44 +1300

    Add some new go files

---
+++ gettingStarted/numeralSystems/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("42:", 42)
+
+	fmt.Printf("%d - %b \n", 42, 42)
+
+	fmt.Printf("%d - %b - %X \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+}
